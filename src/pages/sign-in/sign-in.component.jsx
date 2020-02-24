e1256984752323557bb1469350d08873e35ec43d
yaffi <gkmsnu.14.calm@gmail.com>
2020-02-24 22:09:02 +0900

    サインイン周りの実装

---
+++ src/pages/sign-in/sign-in.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+
+import { SignInContainer } from './sign-in.styles';
+
+const SignIn = () => (
+    <SignInContainer>
+        <SignInForm />
+    </SignInContainer>
+);
+
+export default SignIn;
---
+++ src/pages/sign-in/sign-in.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SignInContainer = styled.div`
+    width: 100%;
+    height: 700px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
