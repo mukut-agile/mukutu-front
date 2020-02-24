0df8c37171ad4f5172309fa1e9b274170a2df90b
yaffi <gkmsnu.14.calm@gmail.com>
2020-02-24 22:06:21 +0900

    Routeの設定とfirebaseのユーザーのログイン状態の管理

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { auth } from './firebase/firebase.urils';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.conponent';
+import SignIn from './pages/sign-in/sign-in.component';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route 
+            exact 
+            path="/signin"
+            render={() => 
+              this.state.currentUser ? <Redirect to='/'/> : <SignIn />}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
