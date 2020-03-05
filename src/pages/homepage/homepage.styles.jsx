ea063329a5e1aed4a33641cc19b72a07273e24e9
yaffi <gkmsnu.14.calm@gmail.com>
2020-03-05 11:57:09 +0900

    タブ名、アイコンの追加

--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -34,6 +34,8 @@ export const HomePageContentContainer = styled.div`
   align-items: center;
   justify-content: center;
   color: white;
+  padding: 10px;
+  background-color: black;
   position: absolute;
 `;
 
