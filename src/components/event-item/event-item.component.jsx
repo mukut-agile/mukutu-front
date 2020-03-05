c197087662f69e2613bd8b03e52e108d36e543cb
yaffi <gkmsnu.14.calm@gmail.com>
2020-03-05 11:24:42 +0900

    イベント一覧画面の修正

--- src/components/event-item/event-item.component.jsx
+++ src/components/event-item/event-item.component.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-import { EventItemContainer, EventItemBackgroundImageContainer, EventItemTitle, EventItemAddress} from './event-item.styles';
+import {
+  EventItemContainer,
+  EventItemBackgroundImageContainer,
+  EventItemTitle,
+  EventItemAddress,
+  EventOverviewContainer
+} from "./event-item.styles";
 
-const EventItem = ({ event_title, place, imageUrl }) => (
+const EventItem = ({
+  event_title,
+  place,
+  imageUrl,
+  start_datetime,
+  overview
+}) => (
   <EventItemContainer>
-    <EventItemBackgroundImageContainer imageUrl={imageUrl}/>
-    <EventItemTitle>{event_title}</EventItemTitle>
-    <EventItemAddress>開催地:{place}</EventItemAddress>
+    <EventItemBackgroundImageContainer imageUrl={imageUrl} />
+    <EventOverviewContainer>
+      <EventItemTitle>{event_title}</EventItemTitle>
+      <EventItemTitle>開催日時:{start_datetime.substr(0, 10)}</EventItemTitle>
+      <EventItemTitle>概要:{overview}</EventItemTitle>
+      <EventItemTitle>開催地:{place}</EventItemTitle>
+    </EventOverviewContainer>
   </EventItemContainer>
 );
 
--- src/components/event-item/event-item.styles.jsx
+++ src/components/event-item/event-item.styles.jsx
@@ -1,29 +1,37 @@
 import styled from 'styled-components';
 
 export const EventItemContainer = styled.div`
-    width: calc(75% / 5 - 0px);
+    width: calc(90% / 2 - 0px);
     height: 90%;
     display: flex;
-    flex-direction: column;
     align-items: center;
     border: solid 1px black;
     margin: 10px 2.5%;
 `
 
 export const EventItemBackgroundImageContainer = styled.div`
-  width: 100%;
-  height: 40%;
+  width: 40%;
+  height: 100%;
   background-size: cover;
   background-position: center;
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `
+
+export const EventOverviewContainer = styled.div`
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 export const EventItemTitle = styled.div`
   width: 90%;
   height: 25%;
   display: flex;
   align-items: center;
   font-weight: bold;
-  font-size: 14px;
+  font-size: 18px;
   color: #444;
 `
 export const EventItemAddress = styled.div`
