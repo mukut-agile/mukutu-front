e548307e0654d7ad3e13a6e6f0436abd3e6b6e49
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-03-05 14:19:07 +0900

    create community_page

---
+++ src/pages/community-page/community-page.styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CommunityPageContainer = styled.div`
+  width: 100%;
+  height: 700px;
+  display: flex;
+  font-size: 60px;
+  color: #4179f7;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
