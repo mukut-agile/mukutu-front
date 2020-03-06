8e490300bc51bd5214a26e6e8fdf314a81231334
yaffi <gkmsnu.14.calm@gmail.com>
2020-03-06 12:31:41 +0900

    Linkを修正

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -10,8 +10,8 @@ export const HeaderContainer = styled.div`
 `;
 
 export const LogoImageContainer = styled.div`
-  width: 100px;
-  height: 100%;
+  width: 100%;
+  height: 100px;
   background-size: cover;
   background-position: center;
   background-image: url('/logo.png');
